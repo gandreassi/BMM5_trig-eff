a0131921bbbd7bd32a9122b7f41002d81dbfc908
Guido Andreassi <guido.andreassi@cern.ch>
2019-09-20 17:10:47 +0200

    convolute.py now fully versatile

--- convolute.py
+++ convolute.py
@@ -4,28 +4,55 @@ from sys import argv
 from math import sqrt
 
 year = argv[1]
+SN = argv[2]
 
 lumi={	"2017" : {	"HLT_Dimuon0_Jpsi" : 75350805.341,
 					"HLT_Dimuon0_Jpsi_NoVertexing" : 55714920.339,
-					#"HLT_DoubleMu4_3_Jpsi_Displaced" : 14479239278.717,
-					"HLT_DoubleMu4_Jpsi_Displaced" : 876993819.962,
+					"HLT_DoubleMu4_3_Jpsi_Displaced" : 14479239278.717,
+					"HLT_DoubleMu4_Jpsi_Displaced" :   876993819.962,
 					"HLT_DoubleMu4_Jpsi_NoVertexing" : 234517906.885},
 		"2018" : {	"HLT_Dimuon0_Jpsi" : 2692017.753,
 					"HLT_Dimuon0_Jpsi_NoVertexing" : 4036006.636,
 					"HLT_DoubleMu4_3_Jpsi" : 1173487158.329,
-					#"HLT_DoubleMu4_Jpsi_Displaced" : 212306014.543,
+					"HLT_DoubleMu4_Jpsi_Displaced" : 212306014.543,
 					"HLT_DoubleMu4_Jpsi_NoVertexing" : 52366197.343}}
+
+
+if (year=="2017" and SN=="S"):
+	trig_A = "HLT_Dimuon0_Jpsi"
+	trig_B = "HLT_Dimuon0_Jpsi_NoVertexing"
+	L1_seed = "1" #take everything
+elif (year=="2017" and SN=="N"):
+	trig_A = "HLT_DoubleMu4_3_Jpsi_Displaced"
+	trig_B = "HLT_DoubleMu4_Jpsi_NoVertexing"
+	L1_seed = "(L1_DoubleMu0er1p5_SQ_OS_dR_Max1p4 || L1_DoubleMu0er1p4_SQ_OS_dR_Max1p4)"
+elif (year=="2018" and SN=="S"):
+	trig_A = "HLT_Dimuon0_Jpsi"
+	trig_B = "HLT_Dimuon0_Jpsi_NoVertexing"
+	L1_seed = "1" #take everything
+elif (year=="2018" and SN=="N"):
+	trig_A = "HLT_DoubleMu4_3_Jpsi"
+	trig_B = "HLT_DoubleMu4_Jpsi_NoVertexing"
+	L1_seed = "(L1_DoubleMu0er1p5_SQ_OS_dR_Max1p4 || L1_DoubleMu0er1p4_SQ_OS_dR_Max1p4)"
+else: raise Exception("ERROR: unexpected combination of year and S/N flag (signal/normalisation).")
 
 chain = r.TChain("Events")
 chain.Add("/eos/user/g/gandreas/jpsikmc/"+year+"/*.root")
+#deactivate useless branches
+chain.SetBranchStatus("*", 0)
+chain.SetBranchStatus("HLT*", 1)
+chain.SetBranchStatus("L1*", 1)
+chain.SetBranchStatus("mm_*", 1)
+chain.SetBranchStatus("bkmm_*", 1)
+chain.SetBranchStatus("Pileup_nTrueInt", 1)
 
-f_map = r.TFile.Open("hists"+year+".root")
+f_map = r.TFile.Open("hists"+year+SN+".root")
 h_pass = f_map.Get("hpass")
 h_pass.Sumw2()
-h_pass.Scale(1./lumi[year]["HLT_DoubleMu4_Jpsi_Displaced"])
+h_pass.Scale(1./lumi[year][trig_A])
 h_tot = f_map.Get("htot")
 h_tot.Sumw2()
-h_tot.Scale(1./lumi[year]["HLT_DoubleMu4_Jpsi_NoVertexing"])
+h_tot.Scale(1./lumi[year][trig_B])
 
 eff_data = h_pass.Clone()
 eff_data.Divide(h_tot)
@@ -33,7 +60,7 @@ eff_data.Divide(h_tot)
 binningX = eff_data.GetXaxis().GetXbins()
 binningY = eff_data.GetYaxis().GetXbins()
 eff_MC = eff_data.Clone("simulation")
-eff_MC = r.TEfficiency("eff_MC","simulation;vertex probability;#epsilon", eff_data.GetNbinsX(), binningX.GetArray(), eff_data.GetNbinsY(), binningY.GetArray())
+eff_MC = r.TEfficiency("eff_MC","simulation ; vertex probability ; vertex detachment significance;", eff_data.GetNbinsX(), binningX.GetArray(), eff_data.GetNbinsY(), binningY.GetArray())
 eff_MC.SetStatisticOption(r.TEfficiency.kBBayesian)
 eff_MC.SetUseWeightedEvents()
 
@@ -50,31 +77,52 @@ MC_PU_h.Scale(1./MC_PU_h.Integral())
 PU_weights = data_PU_h.Clone("PU_w")
 PU_weights.Divide(MC_PU_h)
 
+f_buffer = r.TFile.Open("f_buffer.root", "recreate")
+f_buffer.cd()
+matching_cut_jpsi = "abs(mm_gen_pdgId[0])==443 && abs(mm_gen_mu1_pdgId[0])==13 && abs(mm_gen_mu2_pdgId[0])==13 && (mm_gen_mu1_pdgId[0]*mm_gen_mu2_pdgId[0])<0"
+matching_cut_jpsik = matching_cut_jpsi+" && bkmm_gen_pdgId[0]"
+chain_matched = chain.CopyTree(matching_cut_jpsi)
+del chain
+
+### L1 seed selection
+L1_eff = float(chain_matched.GetEntries(matching_cut_jpsi+" && {0} && {1}".format(trig_B, L1_seed)))/chain_matched.GetEntries(matching_cut_jpsi+" && {0}".format(trig_B))
+eff_data.Scale(1./L1_eff)
+print L1_eff, "<<< this number should be 1 if you are running on S (signal triggers)."
+if SN=="S" and L1_eff!=1:
+	raise Exception("45.961216, 12.976180")
+
+if SN=="N": chain_matched_L1 = chain_matched.CopyTree(L1_seed)
+else: chain_matched_L1 = chain_matched.Clone()
+del chain_matched
+
 n_pass = ufloat(0.,0.)
 n_tot = ufloat(0.,0.)
 
 n_pass_MC = 0.
+L1_tot=0
+L1_pass = 0.
 
-for event in chain:
-	if len(event.mm_kin_vtx_prob)>0:
-		if  abs(event.mm_gen_pdgId[0])==443 \
-			and abs(event.mm_gen_mu1_pdgId[0])==13 and abs(event.mm_gen_mu2_pdgId[0])==13\
-			and event.mm_gen_mu1_pdgId[0]*event.mm_gen_mu2_pdgId[0]<0:
-			#and (event.L1_DoubleMu0er1p5_SQ_OS_dR_Max1p4 or event.L1_DoubleMu0er1p4_SQ_OS_dR_Max1p4)\
-
-			PU_weight = ufloat(PU_weights.GetBinContent(PU_weights.FindBin(chain.Pileup_nTrueInt)), PU_weights.GetBinError(PU_weights.FindBin(chain.Pileup_nTrueInt)))
-			this_vertex_prob = event.mm_kin_vtx_prob[0]
-			this_vertex_det = event.mm_kin_slxy[0]
+#make trigger map on MC
+for event in chain_matched_L1:
+		PU_weight = ufloat(PU_weights.GetBinContent(PU_weights.FindBin(chain_matched_L1.Pileup_nTrueInt)), PU_weights.GetBinError(PU_weights.FindBin(chain_matched_L1.Pileup_nTrueInt)))
+		this_vertex_prob = event.mm_kin_vtx_prob[0]
+		this_vertex_det = event.mm_kin_slxy[0]
+		if getattr(event, trig_B):
+			eff_MC.FillWeighted(getattr(event, trig_A), PU_weight.n, this_vertex_prob, this_vertex_det)
+		if len(event.bkmm_mm_index)>0 and abs(event.bkmm_gen_pdgId[0])==521: #selecting real b2jpsik events
+			mm_index = event.bkmm_mm_index[0]
+			this_vertex_prob = event.mm_kin_vtx_prob[mm_index]
+			this_vertex_det = event.mm_kin_slxy[mm_index]
 			passed = False
-			if event.HLT_DoubleMu4_Jpsi_Displaced:
+			if getattr(event, trig_A):
 				passed = True
 				n_pass_MC += PU_weight
-			if event.HLT_DoubleMu4_Jpsi_NoVertexing:
-				this_bin = eff_data.FindFixBin(this_vertex_prob, this_vertex_det)
+			if getattr(event, trig_B):
+				this_bin = eff_data.FindBin(this_vertex_prob, this_vertex_det)
 				n_pass += ufloat(eff_data.GetBinContent(this_bin), eff_data.GetBinError(this_bin))*PU_weight
 				n_tot += PU_weight
-				eff_MC.FillWeighted(passed, PU_weight.n, this_vertex_prob, this_vertex_det)
-
+				
+f_buffer.Close()
 
 print "++++++++++++++++++++++++++++++++++++++++++++"
 print "Global numbers:"
@@ -110,6 +158,7 @@ c3 = r.TCanvas("c3")
 eff_ratio.Draw("colz")
 c3.SaveAs("plots/eff_ratio"+year+".pdf")
 c3.SaveAs("plots/eff_ratio"+year+".root")
+
 #make difference significance histogram
 eff_diffs = eff_data.Clone()
 for b in range(eff_diffs.GetNbinsX()*eff_diffs.GetNbinsY()):
@@ -124,5 +173,6 @@ c4 = r.TCanvas("c4")
 eff_diffs.Draw("colz")
 c4.SaveAs("plots/eff_diffs"+year+".pdf")
 c4.SaveAs("plots/eff_diffs"+year+".root")
+
 
 #raw_input()
